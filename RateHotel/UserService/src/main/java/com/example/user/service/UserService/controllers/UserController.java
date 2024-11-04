2ee08ca272f8d3dd1b747b548c8d8fbf9c2301a6
sahil <sahilsingh180799@gmail.com>
2024-11-04 23:48:27 +0530

    implemented retry which is also resilience4j lib but servers some different purpose

--- RateHotel/UserService/src/main/java/com/example/user/service/UserService/controllers/UserController.java
+++ RateHotel/UserService/src/main/java/com/example/user/service/UserService/controllers/UserController.java
@@ -3,6 +3,7 @@ package com.example.user.service.UserService.controllers;
 import com.example.user.service.UserService.entities.User;
 import com.example.user.service.UserService.services.UserService;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,9 +26,16 @@ public class UserController {
         User user1 = userService.saveUser(user);
         return ResponseEntity.status(HttpStatus.CREATED).body(user1);
     }
+
+    int retry = 1;
     @GetMapping("/{userId}")
-    @CircuitBreaker(name = "ratingHotelBreaker", fallbackMethod = "ratingHotelFallback")
+    //@CircuitBreaker(name = "ratingHotelBreaker", fallbackMethod = "ratingHotelFallback")
+    @Retry(name = "ratingHotelService",fallbackMethod = "ratingHotelFallback")
     public ResponseEntity<User> getSingleUser(@PathVariable String userId){
+
+        LOGGER.info("Retry Count {}", retry);
+        retry++;
+
          User user = userService.getUser(userId);
          return ResponseEntity.ok(user);
     }
