b16f96c40fa8c119e7aa652e336235183158e980
sahil <sahilsingh180799@gmail.com>
2024-12-14 20:52:56 +0530

    now we are able to produce order event from order service

---
+++ EventDrivenKafkaOrderService/OrderService/src/main/java/com/example/orderservice/controller/OrderController.java
@@ -0,0 +1,32 @@
+package com.example.orderservice.controller;
+
+import com.example.basedomains.dto.Order;
+import com.example.basedomains.dto.OrderEvent;
+import com.example.orderservice.kafka.OrderProducer;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1")
+public class OrderController {
+    private OrderProducer orderProducer;
+
+    public OrderController(OrderProducer orderProducer) {
+        this.orderProducer = orderProducer;
+    }
+
+    public String placeOrder(@RequestBody Order order){
+        order.setOrderId(UUID.randomUUID().toString());
+
+        OrderEvent orderEvent = new OrderEvent();
+        orderEvent.setStatus("Pending");
+        orderEvent.setMessage("Order is in pending state.");
+        orderEvent.setOrder(order);
+
+        orderProducer.sendMessage(orderEvent);
+        return "order Places successfully.";
+    }
+}
