95c5fd04bf79503022904a388a33f84483190c54
sahil <sahilsingh180799@gmail.com>
2025-01-16 00:32:21 +0530

    resolved auth error in docker mongo and not using any username and password for now

--- EcommerceService/ProductService/src/main/java/com/example/productservice/controllers/ProductController.java
+++ EcommerceService/ProductService/src/main/java/com/example/productservice/controllers/ProductController.java
@@ -5,6 +5,7 @@ import com.example.productservice.dto.ProductResponse;
 import com.example.productservice.models.Product;
 import com.example.productservice.services.ProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.*;
@@ -15,7 +16,7 @@ import java.util.List;
 @RequiredArgsConstructor
 @RequestMapping("/api/product")
 public class ProductController {
-
+    @Autowired
     private final ProductService productService;
 
     @PostMapping
--- EcommerceService/ProductService/src/main/java/com/example/productservice/services/ProductService.java
+++ EcommerceService/ProductService/src/main/java/com/example/productservice/services/ProductService.java
@@ -6,6 +6,7 @@ import com.example.productservice.models.Product;
 import com.example.productservice.repositories.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,6 +16,7 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 @Slf4j
 public class ProductService {
+    @Autowired
     private final ProductRepository productRepository;
 
     public ProductResponse createProduct(ProductRequest productRequest){
